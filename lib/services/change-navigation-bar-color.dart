d60e58ebad0cc994f83b677dd1eff005d96e5624
Denis Moiseev <isdenmois@gmail.com>
2021-01-19 17:38:09 +0300

    Revert `changeNavigationBarColor` platform method

--- lib/services/change-navigation-bar-color.dart
+++ lib/services/change-navigation-bar-color.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-// const platform = const MethodChannel('flutter.native/helper');
+const platform = const MethodChannel('flutter.native/helper');
 
 changeNavigationBarColor(Color color, Brightness brightness) {
   final dark = brightness == Brightness.dark;
   final hashColor = '#${color.hashCode.toRadixString(16)}';
 
-  // platform.invokeMethod('changeNavigationBarColor', {'color': hashColor, 'dark': dark});
+  platform.invokeMethod('changeNavigationBarColor', {'color': hashColor, 'dark': dark});
 }
